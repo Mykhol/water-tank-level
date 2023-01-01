024af43b9bdde0561233df1fda34938fbaee488b
Michael Howell <mh.michaelhowell@gmail.com>
2023-01-01 22:50:44 +1300

    update next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  env: {
+    MY_AWS_REGION: process.env.MY_AWS_REGION,
+    MY_AWS_ACCESS_KEY_ID: process.env.MY_AWS_ACCESS_KEY_ID,
+    MY_AWS_SECRET_ACCESS_KEY: process.env.MY_AWS_SECRET_ACCESS_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
