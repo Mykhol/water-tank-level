1b7677bf1fa00639ff1206cf8d9747d5df55a75c
Michael Howell <mh.michaelhowell@gmail.com>
2023-01-01 21:22:33 +1300

    Push all

--- src/lib/di/DI.ts
+++ src/lib/di/DI.ts
@@ -3,9 +3,9 @@ import { WaterLevelRepo } from "../water-level/WaterLevelRepo";
 import { WaterLevel } from "../water-level/WaterLevel";
 
 const dynamoService = new DynamoService<WaterLevel>(
-  process.env.AWS_REGION!,
-  process.env.AWS_ACCESS_KEY_ID!,
-  process.env.AWS_SECRET_ACCESS_KEY!,
+  process.env.MY_AWS_REGION!,
+  process.env.MY_AWS_ACCESS_KEY_ID!,
+  process.env.MY_AWS_SECRET_ACCESS_KEY!,
   "water_tank_level"
 );
 
